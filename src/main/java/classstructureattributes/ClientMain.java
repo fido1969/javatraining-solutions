f684e3242ecf01e004a994b06a7c90f5d5343600
fido1969 <szabadkai.janos@freemail.hu>
2021-03-07 13:29:31 +0100

    ClientMain class was modified and finished

--- src/main/java/classstructureattributes/ClientMain.java
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class ClientMain {
     public static void main(String[] args) {
+        Client client = new Client();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Give your name: ");
-        String name = scanner.nextLine();
+        client.name = scanner.nextLine();
         System.out.print("Give your year of birth: ");
-        int year = scanner.nextInt();
+        client.year = scanner.nextInt();
         scanner.nextLine();
         System.out.print("Give your address: ");
-        String address = scanner.nextLine();
+        client.address = scanner.nextLine();
         System.out.println();
 
-        System.out.println("Your name is " + name);
-        System.out.println("Your year of birth: " + year);
-        System.out.println("Your address: " + address);
+        System.out.println("Your name is " + client.name);
+        System.out.println("Your year of birth: " + client.year);
+        System.out.println("Your address: " + client.address);
     }
 }
