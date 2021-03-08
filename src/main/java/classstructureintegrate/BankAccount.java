b16af5761350ca29499e48f2e9b8e09cea7f1219
fido1969 <szabadkai.janos@freemail.hu>
2021-03-08 13:59:40 +0100

    BankAccount class is finished

---
+++ src/main/java/classstructureintegrate/BankAccount.java
@@ -0,0 +1,33 @@
+package classstructureintegrate;
+
+public class BankAccount {
+
+    private String accountNumber;
+
+    private String owner;
+
+    private int balance;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public String getInfo() {
+        return owner + " (" + accountNumber + "): " + balance + " Ft";
+    }
+
+    public void transfer(BankAccount target, int amount) {
+        balance -= amount;
+        target.balance += amount;
+    }
+}
