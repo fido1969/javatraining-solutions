9b80db7bdf5a9107ab904b04d0d77d4de0eb63c3
fido1969 <szabadkai.janos@freemail.hu>
2021-03-11 20:43:46 +0100

    Languages class is finished

---
+++ src/main/java/week02d02/Languages.java
@@ -0,0 +1,30 @@
+package week02d02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+
+    private List<String> programsList = new ArrayList<>();
+
+    public List<String> ProgramLongerThenFiveChars(List<String> programsList) {
+        List<String> fillterdProgramList = new ArrayList<>();
+        for (String program : programsList) {
+            if (program.length() > 5) {
+                fillterdProgramList.add(program);
+            }
+        }
+        return fillterdProgramList;
+    }
+
+    public static void main(String[] args) {
+
+        Languages languages = new Languages();
+
+        languages.programsList.add("Java");
+        languages.programsList.add("Python");
+        languages.programsList.add("JavaScrip");
+
+        System.out.println(languages.ProgramLongerThenFiveChars(languages.programsList));
+    }
+}
