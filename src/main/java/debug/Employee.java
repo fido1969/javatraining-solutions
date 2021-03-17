1e0a61669717c6ee2e8c6df39dc39c93cd1420e3
fido1969 <szabadkai.janos@freemail.hu>
2021-03-17 22:20:06 +0100

    Employee is finished

---
+++ src/main/java/debug/Employee.java
@@ -0,0 +1,28 @@
+package debug;
+
+public class Employee {
+
+    private String name;
+    private int yearOfBirth;
+
+    public Employee(String name, int yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                '}';
+    }
+}
