a4dc3c5eecf533136c75b750fcd171dfb6fbd799
fido1969 <szabadkai.janos@freemail.hu>
2021-03-17 22:20:59 +0100

    CompanyMain is finished

---
+++ src/main/java/debug/CompanyMain.java
@@ -0,0 +1,18 @@
+package debug;
+
+public class CompanyMain {
+
+    public static void main(String[] args) {
+
+        Company company = new Company();
+        company.addemployee(new Employee("Anna", 1967));
+        company.addemployee(new Employee("Attila", 1878));
+        company.addemployee(new Employee("Béla", 1999));
+        System.out.println(company.listEmployeeNames());
+
+        System.out.println(company.findEmployeeByName("Anna").toString());
+
+        company.addemployee(new Employee("Anonymus", 1805));
+        System.out.println(company.listEmployeeNames());
+    }
+}
