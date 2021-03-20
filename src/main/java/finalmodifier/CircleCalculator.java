f691def15f16d54cc81bce06f148028fe366c28d
fido1969 <szabadkai.janos@freemail.hu>
2021-03-20 12:09:00 +0100

    CircleCalculator class was modified and finished

--- src/main/java/finalmodifier/CircleCalculator.java
+++ src/main/java/finalmodifier/CircleCalculator.java
@@ -2,7 +2,7 @@ package finalmodifier;
 
 public class CircleCalculator {
 
-    public static final double PI = 3.4;
+    public static final double PI = 3.14;
 
     public double calculatePerimeter(double r) {
         return 2 * r * PI;
