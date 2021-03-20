73e4b83a1ad98edae63f33048895419700ee8d79
fido1969 <szabadkai.janos@freemail.hu>
2021-03-20 20:30:55 +0100

    Point class is finished

---
+++ src/main/java/math/Point.java
@@ -0,0 +1,25 @@
+package math;
+
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+}
