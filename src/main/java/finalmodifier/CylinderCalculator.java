1aa2f4e449c05291c4fe2663364f714846dc4585
fido1969 <szabadkai.janos@freemail.hu>
2021-03-20 10:50:36 +0100

    CircleCalculator class is finished

---
+++ src/main/java/finalmodifier/CylinderCalculator.java
@@ -0,0 +1,15 @@
+package finalmodifier;
+
+public class CylinderCalculator {
+
+    public double calculateVolume(double r, double h) {
+
+        return new CircleCalculator().calculateArea(r) * h;
+    }
+
+    public double calculateSurfaceArea(double r, double h) {
+
+        return new CircleCalculator().calculateArea(r) * 2 +
+                new CircleCalculator().calculatePerimeter(r) * h;
+    }
+}
